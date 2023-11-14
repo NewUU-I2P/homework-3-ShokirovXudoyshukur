d3ae8f7e3cdc7bd7c55c31baad3baf3ff814c4f3
ShokirovXudoyshukur <x.shokirov@newuu.uz>
2023-11-14 23:58:22 +0500

    hw3

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,12 +1,12 @@
 float problemSolution1(float consumed_water) {
     float cost;
-    if (consumed_water < -30){
-        cost = (consumed_water * 0.4) + 13}
-    if (consumed_water > 30 and consumed_water <= 50){
-        cost = (consumed_water-30)*0.12+30*0.4+13}
-    if (consumed_water > 50 and consumed_water <= 60){
-        cost = (consumed_water-50)*1.4+30*0.4+ 20*0.12+13}
-    if (consumed_water > 60){
-        cost = (consumed_water-60)*1.5+1.4*10+20*0.12+30*0.4+13}
+    if (consumed_water<=30){
+        cost= (consumed_water*0.4) +13;}
+    if (consumed_water>30 and consumed_water<=50){
+        cost= (consumed_water-30)*0.12 + 12 +13;}
+    if (consumed_water>50 and consumed_water<=60){
+        cost = (consumed_water -50)*1.4+ 12 + 24+13;}
+    if (consumed_water>60){
+        cost= (consumed_water-60)*1.5 + 14 +24 +12+13;}
     return cost;
 }
